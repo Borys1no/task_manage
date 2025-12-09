3a01a28ae29e54a7eb4581e01e933124446ff6ff
Borys1no <boryscereceda@hotmail.com>
2025-12-09 17:38:20 -0500

    Data Base create

--- task/models.py
+++ task/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pendiente'),
+        ('in_progress', 'En progreso'),
+        ('donde', 'Completada'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank= True)
+    status = models.CharField(
+        max_length=20,
+        choices= STATUS_CHOICES,
+        default= 'pending'
+    )
+    user= models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self)-> str:
+        return str (self.title)
+    
