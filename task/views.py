a05e0829d00cd136bb15674e0bab22e8a586f588
Borys1no <boryscereceda@hotmail.com>
2025-12-11 15:40:24 -0500

    Views and Urls complete

--- task/views.py
+++ task/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TaskForm
+from .models import Task
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {
+        'tasks': tasks
+    })
+
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task list')
+    else:
+        form = TaskForm()
+    return render(request, 'task/task_create.html',{
+        'form': form
+    })
