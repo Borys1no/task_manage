20f967fc150acc6f7afae6aa8957c211c612722b
Borys1no <boryscereceda@hotmail.com>
2025-12-15 10:30:16 -0500

    New view: task_edit

--- task/views.py
+++ task/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TaskForm
 from .models import Task
 
@@ -24,3 +24,19 @@ def task_create(request):
     return render(request, 'task/task_create.html',{
         'form': form
     })
+
+
+@login_required
+
+def task_edit(request, id):
+    task = get_object_or_404(Task, id=id, user=request.user)
+    if resquest.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'task/task_edit.html', {'form':form})
+    
